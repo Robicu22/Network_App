c3373fc0482a43b5ae7411783fd03e16ee6594dc
robicu <ilierobert2005@gmail.com>
2026-03-11 13:15:20 +0200

    Basic Net_Sock window (works)

--- src/Main.java
+++ src/Main.java
@@ -3,47 +3,109 @@ import java.awt.event.*;
 import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) {
+    public Process serverProcess;
+    public Process clientProcess;
+    public void main(String[] args) {
 
         JFrame frame = new JFrame("Network Control");
-        frame.setSize(400,300);
+        frame.setSize(500,300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
 
         JButton serverBtn = new JButton("Start Server");
         JButton clientBtn = new JButton("Start Client");
+        JButton stopBtn = new JButton("Stop Server/Client");
+        JTextField ipField = new JTextField(15);
 
         JTextArea log = new JTextArea();
         JScrollPane scroll = new JScrollPane(log);
 
         serverBtn.addActionListener(e -> {
             try {
-                ProcessBuilder pb = new ProcessBuilder("./server");
-                Process p = pb.start();
+                ProcessBuilder pb = new ProcessBuilder("./native/server");
+
+                serverProcess = pb.start();
                 log.append("Server started\n");
+
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(serverProcess.getInputStream()));
+
+                new Thread(() -> {
+                    try {
+                        String line;
+                        while((line = reader.readLine()) != null) {
+                            String finalLine = line;
+
+                            SwingUtilities.invokeLater(() -> {
+                                log.append(finalLine + "\n");
+                            });
+                        }
+                    } catch(Exception ex){
+                        ex.printStackTrace();
+                    }
+                }).start();
             } catch(Exception ex){
-                log.append("Server error\n");
+                ex.printStackTrace();
+                log.append(ex.getMessage() + "\n");
             }
         });
 
         clientBtn.addActionListener(e -> {
             try {
+                if(ipField.getText().equals("")) {log.append("IP is empty\n"); return;}
                 ProcessBuilder pb =
-                        new ProcessBuilder("./client","192.168.1.42");
-                Process p = pb.start();
+                        new ProcessBuilder("./native/client",ipField.getText());
+                clientProcess = pb.start();
                 log.append("Client started\n");
+
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(clientProcess.getInputStream()));
+
+                new Thread(() -> {
+                    try {
+                        String line;
+                        while((line = reader.readLine()) != null) {
+                            String finalLine = line;
+
+                            SwingUtilities.invokeLater(() -> {
+                                log.append(finalLine + "\n");
+                            });
+                        }
+                    } catch(Exception ex){
+                        ex.printStackTrace();
+                    }
+                }).start();
             } catch(Exception ex){
-                log.append("Client error\n");
+                ex.printStackTrace();
+                log.append(ex.getMessage() + "\n");
             }
         });
+
 
         JPanel panel = new JPanel();
         panel.add(serverBtn);
         panel.add(clientBtn);
+        panel.add(ipField);
 
         frame.add(panel,"North");
         frame.add(scroll,"Center");
 
         frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+
+                if(serverProcess != null){
+                    serverProcess.destroy();
+                }
+
+                if(clientProcess != null){
+                    clientProcess.destroy();
+                }
+
+            }
+        });
     }
 }
